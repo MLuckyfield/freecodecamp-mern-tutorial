2ffc2c1afce1de7a4138d1c3bb25779aea5db3f5
MLuckyfield <matt.cyoshida@gmail.com>
2021-09-30 15:12:28 +0900

    resolved mongoose/node compatibility issue (node 12 not compatible with mongoose 6) and updated mongoose connection line per v6 docs

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+//setup middleware
 app.use(cors());
 app.use(express.json());
 
+//connect to Mongodb
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open',() => {
+  console.log('MongoDB connected successfully')
+})
+
+//start listening
 app.listen(port,() => {
   console.log(`Server is running on port: ${port}`)
 })
